14273c4c14171164478ba5f29ea5a068fb49cf77
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-26 17:27:13 +0200

    provide solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+    //access the other arguments by converting them from the arguments[] array
+    const myArguments = Array.from(arguments);
+    myArguments.splice(0, 1);
+    myArguments.sort();
+
+    //should any elements from the arguments be present, remove them using splice()
+    for(let i = 0; i < array.length; i++){
+        for(const argument of myArguments){
+            if(argument === array[i]){
+                array.splice(i, 1)
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
