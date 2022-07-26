436af2b92f95890983809a9574052ff86bff2bad
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-26 16:54:23 +0200

    provide solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(givenString) {
 
+    let string = givenString.toString();
+
+    let result = "";
+
+    for(let i = string.length - 1; i >= 0; i--){
+        
+        result += string.charAt(i);
+    }
+
+    return result;
+
+    
 };
 
 // Do not edit below this line
