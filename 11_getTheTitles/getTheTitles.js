13cfb0b4680ca5e90fc84de9410e1a744dcf05df
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-27 14:44:47 +0200

    provide solution for 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(givenArray) {
 
+    let result = [];
+
+    for(const element of givenArray){
+        result.push(element.title);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
