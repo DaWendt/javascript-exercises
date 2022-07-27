f9c0c7ce5d6952d8cf8a47af291bc468b08e5304
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-27 13:23:37 +0200

    provide solution for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(givenYear) {
+
+    //has to be either devisible by 4 or 400
+    if(givenYear % 100 == 0){
+        if(givenYear % 400 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(givenYear % 4 == 0){
+        return true;
+    }else{
+        return false;
+    }
 
 };
 
