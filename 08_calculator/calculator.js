e0e9fe812d2f55d49ad65f95a35c48348cd10bd3
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-27 13:49:20 +0200

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber - secondNumber;
 };
 
-const sum = function() {
+const sum = function(givenArray) {
 	
+  let sum = 0;
+  for(const number of givenArray){
+    sum += number;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(givenArray) {
+  let product = 1;
+  for(const number of givenArray){
+    product *= number;
+  }
 
+  return product;
 };
 
-const power = function() {
+const power = function(number, power) {
 	
+  let result = number;
+  for(let i = 0; i < power - 1; i++){
+    result *= number;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+
+  if(number == 0){
+    return 1;
+  }
+
+  let result = 1;
+
+  for(let i = number; i > 0; i--){
+    result *= i;
+  }
+
+  return result;
+
 };
 
 // Do not edit below this line
