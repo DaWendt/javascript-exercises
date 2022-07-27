4396ed431fe88b3d9a4c70a12d702e56438ca7fe
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-27 14:41:44 +0200

    provide solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+
+    //convert input to number
+    let theInput = Number(input);
+
+    //return if it is negative
+    if(theInput < 0){
+        return "OOPS";
+    }
+
+    //create the fibanocci sequence, but return at the specified element
+    let fibanocci = [];
+    for(let i=0; i <= theInput; i++){
+        if(i == 0 || i == 1){
+            fibanocci[i] = 1;
+            if(i == theInput - 1){
+                return 1;
+            }
+        }else{
+            fibanocci[i] = fibanocci[i-1] + fibanocci[i-2];
+            if(i == theInput - 1){
+                return fibanocci [i];
+            }
+        }
+    }
 
 };
 
