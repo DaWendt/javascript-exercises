a916e6c0180aa78b5e9a2efb8ef556b5e971d523
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-27 15:11:39 +0200

    provide solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(givenArray) {
 
+    let oldestAge = 0;
+    let result;
+    for(const person of givenArray){
+
+        let dateForSubtraction = [person.yearOfDeath, person.yearOfBirth];
+        if(dateForSubtraction[0] == undefined){
+            dateForSubtraction[0] = new Date().getFullYear();
+        }
+        
+        let age = dateForSubtraction[0] - dateForSubtraction[1];
+
+        if(age > oldestAge){
+            oldestAge = age;
+            result = person;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
